3249ecd5b7a4beec857cdff3fca2537368351b9f
Adriano <adriano.driuzzo@hotmail.com>
2023-12-11 19:08:57 -0300

    sinon

--- greeter.js
+++ greeter.js
@@ -1,12 +1,12 @@
-    function greet(name) {
-        var options = { 
-            weekday: 'long', 
-            year: 'numeric', 
-            month: 'long', 
-            day: 'numeric'
-        }
-        var now = new Date()
-        var formattedDate = now.toLocaleDateString("pt-BR", options)
-        return `Hello, ${name}! Today is ${formattedDate}.`
+function greet(name) {
+    var options = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric'
     }
-module.exports.greet = greet
+    var now = new Date()
+    var formattedDate = now.toLocaleDateString("pt-BR", options)
+    return `Hello, ${name}! Today is ${formattedDate}.`
+}
+module.exports = { greet } 
